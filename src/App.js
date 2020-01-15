fadf1fd1fa971b1aa870e26e3f34ce2a5bba28d0
MohitSinghTomar27 <mtmohit59@gmail.com>
2020-01-15 18:20:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import './App.css';
+import UsersList from './components/users/Users';
+import SingleUser from './components/users/SingleUser';
+import AllPosts from './components/posts/AllPosts';
+import SinglePost from './components/posts/SinglePost';
+import { increment } from './actions/count';
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <ul id="nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/users">Users</Link></li>
+          <li><Link to="/posts">Posts</Link></li>
+          <li><Link to="/">Back</Link></li>
+        </ul>
+        
+
+        <Route path="/users" component={UsersList} exact={true} />
+        <Route path="/posts" component={AllPosts} exact={true} />
+        <Route path="/users/:id" component={SingleUser}/>
+        <Route path="/posts/:id" component={SinglePost} />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+export default connect()(App);
